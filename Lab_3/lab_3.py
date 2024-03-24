563bf2a9933d3e87981434a1173b3f4f89ee2c58
YaroslavZag <yarikzag2005@gmail.com>
2024-03-24 20:20:23 +0200

    Lab_3 update

--- Lab_3/lab_3.py
+++ Lab_3/lab_3.py
@@ -4,8 +4,9 @@ import os
 
 
 class SimpleApp(server.App):
-    title = "Data vizualization"
+    title = "Data visualization"  # Заголовок додатку
 
+    # Визначення вхідних параметрів для користувача
     inputs = [
         {
             "type": "dropdown",
@@ -15,29 +16,32 @@ class SimpleApp(server.App):
                 {"label": "TCI", "value": "TCI"},
                 {"label": "VHI", "value": "VHI"},
             ],
-            "key": "ticker",
-            "action_id": "update_data",
+            "key": "ticker",  # Ключ параметра
+            "action_id": "update_data",  # Ідентифікатор дії для оновлення даних
         },
         {
             "type": "dropdown",
             "label": "Область",
-            "options": [{"label": str(i), "value": str(i)} for i in range(1, 29)],
-            "key": "region",
-            "action_id": "update_data",
+            "options": [{"label": str(i), "value": str(i)} for i in range(1, 29)],  # Опції для регіонів
+            "key": "region",  # Ключ параметра
+            "action_id": "update_data",  # Ідентифікатор дії для оновлення даних
         },
         {
             "type": "text",
             "label": "Рік:",
             "value": "2005",
-            "key": "year",
-            "action_id": "update_data",
+            "key": "year",  # Ключ параметра
+            "action_id": "update_data",  # Ідентифікатор дії для оновлення даних
         },
     ]
 
+    # Визначення кнопки для оновлення даних
     controls = [{"type": "button", "label": "Побудувати", "id": "update_data"}]
 
+    # Визначення вкладок
     tabs = ["Plot", "Table"]
 
+    # Визначення вихідних параметрів (графік та таблиця)
     outputs = [
         {
             "type": "table",
@@ -55,21 +59,24 @@ class SimpleApp(server.App):
         },
     ]
 
+    # Завантажую дані з CSV-файлу
     def getData(self, params):
         current_dir = os.path.dirname(__file__)
         file_path = os.path.join(current_dir, 'data.csv')
         data = pd.read_csv(file_path)
         return data
     
+    # Фільтрую дані з файлу за рокомі регіоном для виводоу в таблиці
     def getDataForYearAndRegion(self, params, year, region):
         data = self.getData(params)
         filtered_data = data[(data['Indexes'] == region) & (data['Year'] == year)]
         return filtered_data
 
+    # Функція для виведення графіку за регіоном, роком і потрібним рядком
     def getPlot(self, params):
         region = int(params['region'])
         ticker = params['ticker']
-        year = int(params['year'])  # Перетворення рядка на ціле число
+        year = int(params['year'])
         data = self.getData(params)
         filtered_data = data[(data['Indexes'] == region) & (data['Year'] == year)]
 
@@ -80,9 +87,10 @@ class SimpleApp(server.App):
         fig = plt_obj.get_figure()
         return fig
     
+    # Вивід таблиці
     def getHTML(self, params):
-        year = int(params['year'])  # Перетворення рядка на ціле число
-        region = int(params['region'])  # Перетворення рядка на ціле число
+        year = int(params['year'])
+        region = int(params['region'])
         data = self.getDataForYearAndRegion(params, year, region)
         return data.to_html()
 
